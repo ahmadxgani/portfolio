f29ba42029cabcbd875cd736bf7d0b01e94b2324
QUERY-GANI <ahmadxgani01@gmail.com>
2021-07-12 19:44:21 +0700

    update navbar layout

--- src/Component/Navigation.js
+++ src/Component/Navigation.js
@@ -2,18 +2,18 @@ import { NavLink } from "react-router-dom";
 
 export default function Navigation() {
     return (
-        <div className="bg-transparent w-full p-5 z-10">
-            <div className="flex justify-between items-center font-classic">
-                <div className="flex justify-between w-32">
-                    <NavLink to="/about" activeClassName="font-bold">About</NavLink>
-                    <NavLink to="/project" activeClassName="font-bold">Project</NavLink>
+        <div className="bg-transparent w-full p-6 z-10">
+            <div className="flex justify-between items-center font-classic text-lg">
+                <div className="flex justify-between w-36">
+                    <NavLink to="/about" activeClassName="font-semibold">About</NavLink>
+                    <NavLink to="/project" activeClassName="font-semibold">Project</NavLink>
                 </div>
-                <div className="flex justify-center flex-col text-center">
-                    <NavLink exact to="/" className="font-title" activeClassName="font-bold">Ahmad Gani</NavLink>
+                <div className="flex justify-center flex-col text-center leading-none">
+                    <NavLink exact to="/" className="font-title" activeClassName="font-semibold">Ahmad Gani</NavLink>
                     <small><code>{"<Web Developer/>"}</code></small>
                 </div>
-                <div className="flex justify-center w-32">
-                    <NavLink to="/connect" activeClassName="font-bold">Connect</NavLink>
+                <div className="text-right w-36">
+                    <NavLink to="/connect" activeClassName="font-semibold">Connect</NavLink>
                 </div>
             </div>
         </div>
