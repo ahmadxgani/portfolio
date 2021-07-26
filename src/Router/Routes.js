afe4bc4df147734cf128411783a26428917f6b4d
QUERY-GANI <ahmadxgani01@gmail.com>
2021-07-26 11:06:52 +0700

    update

--- src/Router/Routes.js
+++ src/Router/Routes.js
@@ -1,22 +1,22 @@
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
-import Navigation from "../Component/Navigation";
-import Banner from "../Component/Banner";
-import About from "../Component/About";
-import Project from "../Component/Project";
-import Connect from "../Component/Connect";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom"
+import Navigation from "../Component/Navigation"
+import Banner from "../Component/Banner"
+import About from "../Component/About"
+import Project from "../Component/Project"
+import Connect from "../Component/Connect"
 
 export default function Routes() {
-    return (
-        <Router>
-            <div className="bg-gray-200">
-                <Navigation/>
-                <Switch>
-                    <Route exact path="/" component={Banner} />
-                    <Route exact path="/project" component={Project} />
-                    <Route exact path="/connect" component={Connect} />
-                    <Route exact path="/about" component={About} />
-                </Switch>
-            </div>
-        </Router>
-    )
+  return (
+    <Router>
+      <div className="bg-gray-200">
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={Banner} />
+          <Route exact path="/project" component={Project} />
+          <Route exact path="/connect" component={Connect} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
