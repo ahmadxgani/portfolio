93b3b4da6f5e7c4866b38bfa6f7c16df0df6406e
QUERY-GANI <ahmadxgani01@gmail.com>
2021-07-26 11:48:50 +0700

    edit navbar

--- src/Component/Navigation.js
+++ src/Component/Navigation.js
@@ -2,27 +2,17 @@ import { NavLink } from "react-router-dom"
 
 export default function Navigation() {
   return (
-    <div className="bg-transparent w-full p-6 z-10">
-      <div className="flex justify-between items-center font-classic text-lg">
-        <div className="flex justify-between w-36">
-          <NavLink to="/about" activeClassName="font-semibold">
-            About
-          </NavLink>
+    <div className="bg-transparent w-full p-5 z-10">
+      <div className="flex justify-between items-center text-lg font-title">
+        <NavLink exact to="/" activeClassName="font-semibold">
+          Homepage
+        </NavLink>
+        <div className="flex justify-between gap-5">
           <NavLink to="/project" activeClassName="font-semibold">
-            Project
+            My CV
           </NavLink>
-        </div>
-        <div className="flex justify-center flex-col text-center leading-none">
-          <NavLink exact to="/" className="font-title" activeClassName="font-semibold">
-            Ahmad Gani
-          </NavLink>
-          <small>
-            <code>{"<Web Developer/>"}</code>
-          </small>
-        </div>
-        <div>
           <NavLink to="/connect" activeClassName="font-semibold">
-            Connect with me
+            Contact me
           </NavLink>
         </div>
       </div>
